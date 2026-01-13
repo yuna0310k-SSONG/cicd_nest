a8be650ab3c742bfb6907cf6b582c5f776710a31
Yuna <una3917@naver.com>
2026-01-13 15:00:24 +0900

    수정완3

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,14 +3,28 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Student } from '../src/students/entities/student.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
+    const mockRepo = {
+      create: jest.fn(),
+      save: jest.fn(),
+      find: jest.fn(),
+      findOne: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    };
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(getRepositoryToken(Student))
+      .useValue(mockRepo)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
